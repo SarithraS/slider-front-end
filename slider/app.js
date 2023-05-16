5789192948cb23cbb6dde74536cb922700cd1199
SARITHRA S M <128618271+SarithraS@users.noreply.github.com>
2023-05-16 12:55:27 +0530

    Add files via upload

---
+++ slider/app.js
@@ -0,0 +1,29 @@
+const slides=document.querySelectorAll(".slide")
+var counter=0;
+//console.log(slides) 
+slides.fortach(
+    (slide,index) => {
+        slide.style.left = '$(index * 100)%'
+    }
+)
+    const goPrev = () => {
+        counter--
+        
+        slideImage()
+    }
+    
+
+const goNext = () => {
+    counter++
+    
+    slideImage()
+}
+
+
+const slideImage=() => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = 'translateX(-${counter*100}%)'
+        }
+    )
+}
